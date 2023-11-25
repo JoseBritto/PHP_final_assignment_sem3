d54d3a27057b8edd6869bec47c0a1715e312aab4
Ryu-JDT <john0999912@gmail.com>
2023-11-25 17:48:56 -0500

    uh

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="icon" type="image/x-icon">
+    <link rel="icon" type="image/x-icon" href="web-design.png">
     <link rel="stylesheet" href="assets/css/global.css">
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
