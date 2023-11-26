7ce10b1da70749c823b006c2ff8480ab2e0ff56a
Ryu-JDT <john0999912@gmail.com>
2023-11-25 23:16:56 -0500

    bro idk

---
+++ home.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/home.css">
+  <title>Left-aligned Navigation with Header</title>
+</head>
+<body>
+
+<header>
+    <h1>Your Website Name</h1>
+    <div class="header-right">
+      <a href="#">My Account</a>
+      <a href="#" id="settingsLink">Settings</a>
+    </div>
+  </header>
+<div class="headleft">
+  <nav class="leftnav">
+    <ul>
+      <li><a href="#">Dashboard</a></li>
+      <li><a href="#">Pathways</a></li>
+      <li><a href="#">Badges</a></li>
+      <li><a href="#">Certificates</a></li>
+      <li><a href="#">Curriculum Vitae</a></li>
+    </ul>
+  </nav>
+  </div>
+
+  <article>
+    <h1>What is Pathways</h1>
+    <p>learning program that will help you navigate through getting a job, <br> be a successful employee, and plan for your future bitch.</p>
+  </article>
+    <div class="mid_nav">
+        <button class="all">All</button>
+        <button class="in_progress">In progress</button>
+        <button class="Finished">Finished</button>
+    </div>
+    <div class="grid-container">
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+        <div class="grid-item">
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">      
+        </div>
+
+    </div>
+
+</body>
+</html>
