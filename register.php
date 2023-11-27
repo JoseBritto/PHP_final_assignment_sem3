450d9540277e038de55012d7fedd6d893d6fd438
britto <josebrittosaaji@gmail.com>
2023-11-27 14:45:38 -0500

    Redesign register page to be like login

--- register.php
+++ register.php
@@ -1,36 +1,57 @@
-<!DOCTYPE html>
+
+<!--Redirect if logged-in, maybe?-->
+
+<!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/register.css">
-  <title>User Registration</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign up</title>
+    <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
-  <div class="container">
-    <form action="register.php" method="post" class="items">
-      <h2>User Registration</h2>
 
-      <label for="fname">First Name:</label>
-      <input type="text" id="fname" name="fname" required>
+<div class="img-box">
+    <img src="assets/css/img/web-design.png" alt="" width="256">
+</div>
 
-      <label for="lname">Last Name:</label>
-      <input type="text" id="lname" name="lname" required>
+<div class="login-box">
+    <h1>Sign Up</h1>
 
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required>
+    <h2>Register blah bla blah!</h2>
+   
+    <form action="register.php" method="post">
+        <div class="input-field">
+            <input type="text" id="username" name="username" required>
+            <label for="username">Username</label>
+            <div class="underline"></div>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="password" name="password" id="password" required>
+            <label for="password">Password </label>
+            <div class="underline"></div>
+        </div>
+        <div class="input-field">
+            <input type="password" name="confirm_password" id="confirm_password" required>
+            <label for="confirm_password">Confirm Password </label>
+            <div class="underline"></div>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="text" name="display_name" id="display_name" required>
+            <label for="display_name">Display Name </label>
+            <div class="underline"></div>
+        </div>
+        <br>
+        <input class="submit" type="submit" value="Register Now">
+        <p>Already have an account? <a href="https://example.com">Login</a>.</p>
 
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required>
-
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
-
-      <label for="confirmPassword">Confirm Password:</label>
-      <input type="password" id="confirmPassword" name="confirmPassword" required>
-
-      <button type="submit">Register</button>
     </form>
-  </div>
+</div>
 </body>
 </html>
+
