150c1fbe62e3326f4a51d2833ec9b0807a7192e1
Ryu-JDT <john0999912@gmail.com>
2023-11-27 18:09:10 -0500

    aidbsid

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/css/home.css">
-  <title>Left-aligned Navigation with Header</title>
+  <title>PEPE_OnTop</title>
 </head>
 
 <body>
@@ -43,8 +43,13 @@
         <button class="Finished">Finished</button>-->
     </div>
     <div class="grid-container">
-
-        <div class="grid-item">
+      <div class="grid-item">
+        <div class="container" onclick="myFunction(this)">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+        </div>
+        
           <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
           <h4>PATHWAY</h4>
           <h2>Getting a job</h2>
@@ -52,18 +57,20 @@
             <div>
               <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
             </div>     
-        </div>
+      </div>
 
         <div class="grid-item">
+
           <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
           <h4>PATHWAY</h4>
           <h2>Getting a job</h2>
-          <progress value="20" max="100"></progress>
+          <progress value="70" max="100"></progress>
             <div>
               <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
             </div>     
         </div>
         <div class="grid-item">
+
           <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
           <h4>PATHWAY</h4>
           <h2>Getting a job</h2>
@@ -101,6 +108,11 @@
         </div>
 
     </div>
+    <script>
+function myFunction(x) {
+  x.classList.toggle("change");
+}
 
+</script>
 </body>
 </html>
