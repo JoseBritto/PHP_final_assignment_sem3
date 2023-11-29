86d1ef26f38433ddb5bf4ea6694a8511dd212cd7
britto <josebrittosaaji@gmail.com>
2023-11-29 16:50:39 -0500

    Fixed inconsitency between browsers for pathway cards

--- home.php
+++ home.php
@@ -3,7 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="stylesheet" href="assets/css/reset-all.css">
+    <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="stylesheet" href="assets/css/components/pathway-card.css">
+    
   <title>PEPE_OnTop</title>
 </head>
 
