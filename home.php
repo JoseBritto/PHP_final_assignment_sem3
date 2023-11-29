0cff8a061a2d531cb5ce726ad84dfa9baa2085b7
Ryu-JDT <john0999912@gmail.com>
2023-11-28 20:41:53 -0500

    jh,vgjvuy

--- home.php
+++ home.php
@@ -11,9 +11,13 @@
 <header>
     <h1>Pathways</h1>
     <div class="header-right">
-      <span onclick="buttonClick" href="#">My Account</span>
-    
-    </div>
+    <button class="drop_btn">My Account</button>
+      <div class="drop_down-content">
+        <a href="#" class="profile">Profile</a><hr>
+        <a href="#" class="settings">Settings</a><hr>
+        <a href="#" class="logout">Logout</a>
+      </div>
+</div>
   </header>
 <div class="headleft">
   <nav class="leftnav">
@@ -38,81 +42,145 @@
       <li><a href="#" class="Finished">Finished</a></li>
     </ul>
 
-       <!--<button class="all">All</button>
-        <button class="in_progress">In progress</button>
-        <button class="Finished">Finished</button>-->
+      
+    
     </div>
     <div class="grid-container">
-      <div class="grid-item">
-        <div class="container" onclick="myFunction(this)">
-            <div class="bar1"></div>
-            <div class="bar2"></div>
-            <div class="bar3"></div>
+    <!-- Grid item 1 -->
+    <div class="grid-item">
+      <div class="dropdown">
+        <button onclick="myFunction('dropdown1')" class="dropbtn">&vellip;</button>
+        <div id="dropdown1" class="dropdown-content">
+          <a href="#" class="completed">Completed</a>
+          <a href="#" class="remove">Remove</a>
         </div>
-        
-          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
-          <h4>PATHWAY</h4>
-          <h2>Getting a job</h2>
-          <progress value="20" max="100"></progress>
-            <div>
-              <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
-            </div>     
       </div>
-
-        <div class="grid-item">
-
-          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
-          <h4>PATHWAY</h4>
-          <h2>Getting a job</h2>
-          <progress value="70" max="100"></progress>
-            <div>
-              <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
-            </div>     
-        </div>
-        <div class="grid-item">
-
-          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
-          <h4>PATHWAY</h4>
-          <h2>Getting a job</h2>
-          <progress value="20" max="100"></progress>
-            <div>
-              <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
-            </div>     
-        </div>
-        <div class="grid-item">
-          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
-          <h4>PATHWAY</h4>
-          <h2>Getting a job</h2>
-          <progress value="20" max="100"></progress>
-            <div>
-              <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
-            </div>     
-        </div>
-        <div class="grid-item">
-          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
-          <h4>PATHWAY</h4>
-          <h2>Getting a job</h2>
-          <progress value="20" max="100"></progress>
-            <div>
-              <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
-            </div>     
-        </div>
-        <div class="grid-item">
-          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
-          <h4>PATHWAY</h4>
-          <h2>Getting a job</h2>
-          <progress value="20" max="100"></progress>
-            <div>
-              <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.<p>
-            </div>     
-        </div>
-
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
+      <h4>PATHWAY</h4>
+      <h2>Getting a job</h2>
+      <progress value="20" max="100"></progress>
+      <div>
+        <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.</p>
+      </div>
     </div>
-    <script>
-function myFunction(x) {
-  x.classList.toggle("change");
-}
 
-</script>
+    <!-- Grid item 2 -->
+    <div class="grid-item">
+      <div class="dropdown">
+        <button onclick="myFunction('dropdown2')" class="dropbtn">&vellip;</button>
+        <div id="dropdown2" class="dropdown-content">
+          <a href="#" class="completed">Completed</a>
+          <a href="#" class="remove">Remove</a>
+        </div>
+      </div>
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
+      <h4>PATHWAY</h4>
+      <h2>Getting a job</h2>
+      <progress value="20" max="100"></progress>
+      <div>
+        <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.</p>
+      </div>
+    </div>
+
+    <!-- Grid item 3 -->
+    <div class="grid-item">
+      <div class="dropdown">
+        <button onclick="myFunction('dropdown3')" class="dropbtn">&vellip;</button>
+        <div id="dropdown3" class="dropdown-content">
+          <a href="#" class="completed">Completed</a>
+          <a href="#" class="remove">Remove</a>
+        </div>
+      </div>
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
+      <h4>PATHWAY</h4>
+      <h2>Getting a job</h2>
+      <progress value="20" max="100"></progress>
+      <div>
+        <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.</p>
+      </div>
+    </div>
+
+    <!-- Grid item 4 -->
+    <div class="grid-item">
+      <div class="dropdown">
+        <button onclick="myFunction('dropdown4')" class="dropbtn">&vellip;</button>
+        <div id="dropdown4" class="dropdown-content">
+          <a href="#" class="completed">Completed</a>
+          <a href="#" class="remove">Remove</a>
+        </div>
+      </div>
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
+      <h4>PATHWAY</h4>
+      <h2>Getting a job</h2>
+      <progress value="20" max="100"></progress>
+      <div>
+        <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.</p>
+      </div>
+    </div>
+
+    <!-- Grid item 5 -->
+    <div class="grid-item">
+      <div class="dropdown">
+        <button onclick="myFunction('dropdown5')" class="dropbtn">&vellip;</button>
+        <div id="dropdown5" class="dropdown-content">
+          <a href="#" class="completed">Completed</a>
+          <a href="#" class="remove">Remove</a>
+        </div>
+      </div>
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
+      <h4>PATHWAY</h4>
+      <h2>Getting a job</h2>
+      <progress value="20" max="100"></progress>
+      <div>
+        <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.</p>
+      </div>
+    </div>
+
+    <!-- Grid item 6 -->
+    <div class="grid-item">
+      <div class="dropdown">
+        <button onclick="myFunction('dropdown6')" class="dropbtn">&vellip;</button>
+        <div id="dropdown6" class="dropdown-content">
+          <a href="#" class="completed">Completed</a>
+          <a href="#" class="remove">Remove</a>
+        </div>
+      </div>
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="img">
+      <h4>PATHWAY</h4>
+      <h2>Getting a job</h2>
+      <progress value="20" max="100"></progress>
+      <div>
+        <p> In a quaint town nestled between rolling hills, Sarah discovered an ancient bookshop that seemed untouched by time. Intrigued, she entered and found a mysterious tome bound in worn leather. As she opened it, the pages whispered forgotten tales of mystical lands, unveiling a world of enchantment and adventure she never imagined.</p>
+      </div>
+    </div>
+  </div>
+  <script>
+    // Keep track of the currently open dropdown
+    var openDropdown = null;
+
+    function myFunction(id) {
+      var dropdown = document.getElementById(id);
+      if (openDropdown && openDropdown !== dropdown) {
+        openDropdown.classList.remove('show');
+      }
+
+      dropdown.classList.toggle('show');
+      openDropdown = dropdown;
+    }
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function (event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    };
+  </script>
 </body>
 </html>
