42b8abbf74affb678b3dd11305511943bcb80f70
britto <josebrittosaaji@gmail.com>
2023-11-29 20:02:23 -0500

    Header now separate component Also home page refactor

---
+++ pathway.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PATHWAY_NAME</title>
+</head>
+<body>
+    <header>
+        <h1>PATHWAY_NAME</h1>
+        <div class="header-right">
+            <button class="my_acc_drop_btn">My Account</button>
+            <div class="drop_down-content">
+                <a href="#" class="profile">Profile</a><hr>
+                <a href="#" class="settings">Settings</a><hr>
+                <a href="#" class="logout">Logout</a>
+          </div>
+        </div>
+    </header>
+</body>
+</html>
