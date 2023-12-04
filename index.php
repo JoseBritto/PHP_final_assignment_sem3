f6e0463f0e8b24a8e25ae4586c533394cbd34cef
britto <josebrittosaaji@gmail.com>
2023-12-03 21:25:02 -0500

    redirec

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-require "utils/config.php";
-
-echo "Welcome! <br/>";
-
-
-
+// redirect to home.php
+Header("Location: home.php");
+/*
 // Create connection using mysqli_connect function
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 // Connection Check
@@ -15,4 +12,4 @@ if (!$conn) {
     echo "Connected Successfully!";
     $conn->close();
 }
-
+*/
